e52b0ad32f078f41ed5e273cbf5b9a59e38bf367
John Guacaran <john.guacaran@globant.com>
2023-10-18 13:26:57 -0300

    prueba

---
+++ force-app/main/default/lwc/componentedePermisos/componentedePermisos.js
@@ -0,0 +1,40 @@
+import { LightningElement } from "lwc";
+
+export default class ComponentedePermisos extends LightningElement {
+
+    employeeColumns = [
+        { label: 'Employee Id', fieldName: 'employeeId' },
+        { label: 'First Name', fieldName: 'firstName' },
+        { label: 'Last Name', fieldName: 'lastName' },
+        { label: 'Phone Number', fieldName: 'employeePhone', type: 'phone' },
+        { label: 'Email Address', fieldName: 'employeeEmail', type: 'email' },
+        { label: 'Perfil', fieldName: 'Supervisor', type: 'text' }
+    ];
+
+    employeeData = [
+        {
+            employeeId: '1',
+            firstName: 'Richard',
+            lastName: 'Hendricks',
+            employeePhone: '(158) 389-2794',
+            employeeEmail: 'richard@piedpiper.com',
+            Supervisor: 'Supervisor'
+        },
+        {
+            employeeId: '2',
+            firstName: 'Jared',
+            lastName: 'Dunn',
+            employeePhone: '(518) 390-2749',
+            employeeEmail: 'jared@piedpiper.com',
+            Supervisor: 'Supervisor'
+        },
+        {
+            employeeId: '3',
+            firstName: 'Erlich',
+            lastName: 'Bachman',
+            employeePhone: '(815) 391-2974',
+            employeeEmail: 'erlich.bachman@piedpiper.com',
+            Supervisor: 'Supervisor'
+        }
+    ];
+}
